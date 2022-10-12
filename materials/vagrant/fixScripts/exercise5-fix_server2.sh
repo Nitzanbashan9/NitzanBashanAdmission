d00d5464cc528b164a29077e2ef41f226f126a2b
Nitzan <>
2022-10-12 22:00:20 +0300

    combine file

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+echo "192.168.60.10 server1" | sudo tee -a /etc/hosts
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo service sshd restart
+
+
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
 echo "vagrant" >pass.txt
 sudo apt-get install sshpass
