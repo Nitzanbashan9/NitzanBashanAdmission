148873155ae399df3159a201a4b6fab9b7eeb663
Nitzan <>
2022-10-12 22:12:48 +0300

    adding update apt-get

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -7,5 +7,6 @@ sudo service sshd restart
 
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
 echo "vagrant" >pass.txt
+sudo apt-get update
 sudo apt-get install sshpass
 sshpass -f pass.txt  ssh-copy-id -i ~/.ssh/id_rsa.pub vagrant@server1 -o StrictHostKeyChecking=no
