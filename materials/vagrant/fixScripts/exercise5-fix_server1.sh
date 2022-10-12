6708ff8e319288a950adeb3ecb0d1ceae032f844
Nitzan <>
2022-10-12 21:38:57 +0300

    fix 5 server1

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+
+#generate ssh key
+ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
+
+#workaround in order to insert password in a script
+echo "vagrant" >pass.txt
+sudo apt-get install sshpass
+
+#update authorized_keys file in server2
+sshpass -f pass.txt  ssh-copy-id -i ~/.ssh/id_rsa.pub vagrant@server2 -o StrictHostKeyChecking=no
